6447d43100d075782b2b4f47dc3183d8e3862acb
Nigel Rahkola <me@nigelrahkola.com>
2018-09-30 19:37:14 -0400

    Add first pass solution to 2015d12

--- js_abacus_framework/js_abacus_framework.go
+++ js_abacus_framework/js_abacus_framework.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -23,6 +24,47 @@ func sum(json string) int {
 	return count
 }
 
+func sumJSONWithIgnore(data []byte) int {
+	var f interface{}
+	err := json.Unmarshal(data, &f)
+	if err != nil {
+		log.Fatalln("Failed unmarshalling json", err)
+	}
+	sum := 0
+	traverseSum(f, &sum)
+	return int(sum)
+}
+
+func traverseSum(json interface{}, sum *int) {
+	switch vv := json.(type) {
+	case string:
+	case float64:
+		*sum = *sum + int(vv)
+	case []interface{}:
+		for _, v := range vv {
+			traverseSum(v, sum)
+		}
+	case map[string]interface{}:
+		ignore := false
+		for _, v := range vv {
+			switch t := v.(type) {
+			case string:
+				if t == "red" {
+					ignore = true
+				}
+			}
+		}
+		if ignore == true {
+			return
+		}
+		for _, v := range vv {
+			traverseSum(v, sum)
+		}
+	default:
+		fmt.Println(vv, "is of a type I don't know how to handle")
+	}
+}
+
 func inputLine() string {
 	file, err := os.Open("/Users/nrahkola/go/src/github.com/jollyra/" +
 		"advent-of-go/js_abacus_framework/input.txt")
@@ -37,5 +79,7 @@ func inputLine() string {
 }
 
 func main() {
-	fmt.Println(sum(inputLine()))
+	input := inputLine()
+	fmt.Println("Part 1:", sum(input))
+	fmt.Println("Part 2:", sumJSONWithIgnore([]byte(input)))
 }
--- js_abacus_framework/js_abacus_framework_test.go
+++ js_abacus_framework/js_abacus_framework_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestIncrement(t *testing.T) {
+func TestSum(t *testing.T) {
 	cases := []struct {
 		in   string
 		want int
@@ -21,6 +21,24 @@ func TestIncrement(t *testing.T) {
 		got := sum(c.in)
 		if got != c.want {
 			t.Errorf("sum(%q) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
+
+func TestSumJSONWithIgnore(t *testing.T) {
+	cases := []struct {
+		in   string
+		want int
+	}{
+		{"[1,2,3]", 6},
+		{`[1,{"c":"red","b":2},3]`, 4},
+		{`{"d":"red","e":[1,2,3,4],"f":5}`, 0},
+		{`[1,"red",5]`, 6},
+	}
+	for _, c := range cases {
+		got := sumJSONWithIgnore([]byte(c.in))
+		if got != c.want {
+			t.Errorf("sumJSON(%v) == %d, want %d", c.in, got, c.want)
 		}
 	}
 }
