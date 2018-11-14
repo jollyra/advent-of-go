8b1d88b1fcdb549f57169cd992f923fc2b200750
Nigel Rahkola <me@nigelrahkola.com>
2018-11-13 19:44:13 -0500

    Update the advent template

--- template.go
+++ template.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	// "github.com/jollyra/stringutil"
 	// "github.com/jollyra/numutil"
+	// "github.com/jollyra/go-advent-util"
 )
 
 func main() {
+	// lines := advent.InputLines(advent.MustGetArg(1))
 	fmt.Println("Hello")
 }
