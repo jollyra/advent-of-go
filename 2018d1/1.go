6daab5faf1705a7b6e51cbc5940d7356d12a60d6
Nigel Rahkola <me@nigelrahkola.com>
2018-12-01 11:47:56 -0500

    Refactor 2015d1

--- 2018d1/1.go
+++ 2018d1/1.go
@@ -2,32 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/jollyra/go-advent-util"
 	"os"
-	"strconv"
-	"strings"
+
+	"github.com/jollyra/go-advent-util"
 )
+
+func freq(freqs []int) int {
+	freq := 0
+	for _, i := range freqs {
+		freq += i
+	}
+	return freq
+}
+
+func firstRepeat(freqs []int) int {
+	seen := make(map[int]int)
+	freq := 0
+	for {
+		for _, f := range freqs {
+			seen[freq]++
+			if seen[freq] == 2 {
+				return freq
+			}
+			freq += f
+		}
+	}
+}
 
 func main() {
 	lines := advent.InputLines(os.Args[1])
-
-	var sum int
-	for _, line := range lines {
-		i, _ := strconv.Atoi(strings.TrimSpace(line))
-		sum += i
-	}
-	fmt.Println("part 1:", sum)
-
-	seen := make(map[int]int)
-	for {
-		for _, line := range lines {
-			seen[sum]++
-			if seen[sum] == 2 {
-				fmt.Println("part 2:", sum)
-				return
-			}
-			i, _ := strconv.Atoi(strings.TrimSpace(line))
-			sum += i
-		}
-	}
+	freqs := advent.LinesToInts(lines)
+	fmt.Println("Part 1: Ended on frequency", freq(freqs))
+	fmt.Println("Part 2: First repeated freqency", firstRepeat(freqs))
 }
