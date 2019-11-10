588c56ee114ac3970f66f66e08f183a6aceb750e
Nigel Rahkola <me@nigelrahkola.com>
2019-11-10 15:57:25 -0500

    Begin water DFS
    This isn't a working commit

--- 2018d17/17.go
+++ 2018d17/17.go
@@ -5,6 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+)
+
+const (
+	SPRING  = '+'
+	SAND    = '.'
+	CLAY    = '#'
+	WETSAND = '|'
+	WATER   = '~'
 )
 
 type Point struct{ X, Y int }
@@ -29,7 +38,7 @@ func inputLines(fn string) []string {
 
 func parseCoords(lines []string) Coords {
 	coords := make(map[Point]rune)
-	coords[Point{X: 500, Y: 0}] = '+' // Add the water source
+	coords[Point{X: 500, Y: 0}] = SPRING // Add the water source
 	for _, line := range lines {
 		var x0, x1, y0, y1 int
 		ny, _ := fmt.Sscanf(line, "x=%d, y=%d..%d", &x0, &y0, &y1)
@@ -37,12 +46,12 @@ func parseCoords(lines []string) Coords {
 		if ny == 3 {
 			for y := y0; y <= y1; y++ {
 				p := Point{x0, y}
-				coords[p] = '#'
+				coords[p] = CLAY
 			}
 		} else if nx == 3 {
 			for x := x0; x <= x1; x++ {
 				p := Point{x, y0}
-				coords[p] = '#'
+				coords[p] = CLAY
 			}
 		} else {
 			panic("Sscanf failed")
@@ -66,6 +75,36 @@ func render(coords Coords, topLeft, botRight Point) {
 	}
 }
 
+func waterDFS(src Point, coords Coords) Coords {
+	// TODO remove next 2 lines
+	topLeft := Point{X: 494, Y: 0}
+	botRight := Point{X: 507, Y: 13}
+
+	horizon := make([]Point, 0, 0)
+	horizon = append(horizon, src)
+	for len(horizon) > 0 {
+		cur, horizon := horizon[0], horizon[1:]
+		fmt.Println("cur:", cur, "horizon:", horizon)
+		switch groundType := coords[cur]; groundType {
+		case SPRING:
+			horizon = append(horizon, Point{cur.X, cur.Y + 1})
+		case WETSAND:
+			horizon = append(horizon, Point{cur.X, cur.Y + 1})
+		case CLAY:
+			horizon = append(horizon, Point{cur.X + 1, cur.Y})
+			horizon = append(horizon, Point{cur.X - 1, cur.Y})
+			coords[cur] = WATER
+		default:
+			horizon = append(horizon, Point{cur.X, cur.Y + 1})
+			coords[cur] = WETSAND
+		}
+		fmt.Println(horizon)
+		render(coords, topLeft, botRight)
+		time.Sleep(500 * time.Millisecond)
+	}
+	return coords
+}
+
 func main() {
 	fmt.Println("Day 17 part 1")
 	lines := inputLines("17_test.in")
@@ -73,4 +112,5 @@ func main() {
 	topLeft := Point{X: 494, Y: 0}
 	botRight := Point{X: 507, Y: 13}
 	render(coords, topLeft, botRight)
+	waterDFS(Point{500, 0}, coords)
 }
