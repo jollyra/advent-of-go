89edaf375435d0847bfa6685bbaddfe2b71b6efa
Nigel Rahkola <me@nigelrahkola.com>
2020-12-01 20:54:01 -0800

    Use advent util

--- 2020d1/1.go
+++ 2020d1/1.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
 	"strconv"
+
+	"github.com/jollyra/go-advent-util"
 )
-
-func inputLines(filename string) []string {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines
-}
 
 func ints(ss []string) (xs []int) {
 	for _, s := range ss {
@@ -37,7 +19,7 @@ func ints(ss []string) (xs []int) {
 }
 
 func main() {
-	lines := inputLines("1.in")
+	lines := advent.InputLines("1.in")
 	xs := ints(lines)
 	for i := range xs {
 		for j := range xs {
